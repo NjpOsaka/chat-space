b6894b5aa6777ec306d57bd4fd32731787070ac5
NjpOsaka <n.jposaka@gmail.com>
2020-02-13 17:43:37 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.1"
+lock "~> 3.12.0"
 
 set :application, "chat-space"
 set :repo_url,  'git@github.com:N.jpOsaka/chat-space.git'
